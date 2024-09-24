39cc6db8ca5ee9e6a7140a5fba2cf35ebe4d18c3
fox-bones <mrcaysizzle@gmail.com>
2024-09-24 15:52:42 -0400

    added search and styling to individual

--- PDMasterDetail/Pages/Cayce/Index.cshtml.cs
+++ PDMasterDetail/Pages/Cayce/Index.cshtml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PDMasterDetail.Data;
 using PDMasterDetail.Models;
@@ -19,11 +21,34 @@ namespace PDMasterDetail.Pages.Cayce
             _context = context;
         }
 
-        public IList<RockClass> RockClass { get;set; } = default!;
+        public IList<RockClass> RockClass { get; set; } = default!;
 
         public async Task OnGetAsync()
         {
-            RockClass = await _context.RockClass.ToListAsync();
+            // Use LINQ to get list of genres.
+            IQueryable<string> rockQuery = from r in _context.RockClass
+                                            orderby r.Type
+                                            select r.Type;
+            var rocks = from r in _context.RockClass
+                         select r;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                rocks = rocks.Where(s => s.Name.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(RockType))
+            {
+                rocks = rocks.Where(x => x.Type == RockType);
+            }
+            Type = new SelectList(await rockQuery.Distinct().ToListAsync());
+            RockClass = await rocks.ToListAsync();
         }
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Type { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? RockType { get; set; }
     }
 }
--- PDMasterDetail/obj/Debug/net8.0/PDMasterDetail.AssemblyInfo.cs
+++ PDMasterDetail/obj/Debug/net8.0/PDMasterDetail.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PDMasterDetail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753013d390787890ea059b38a12632eabcfab1d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d041e914350e19424afbb8d02b6117b4c8f279f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("PDMasterDetail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PDMasterDetail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
