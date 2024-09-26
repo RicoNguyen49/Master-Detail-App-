38eb2cb9eb56fd71c2fec2d1a832fa53a04faea1
fox-bones <mrcaysizzle@gmail.com>
2024-09-26 16:55:11 -0400

    completed index/details pages

--- PDMasterDetail/obj/Debug/net8.0/PDMasterDetail.AssemblyInfo.cs
+++ PDMasterDetail/obj/Debug/net8.0/PDMasterDetail.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PDMasterDetail")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d041e914350e19424afbb8d02b6117b4c8f279f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39cc6db8ca5ee9e6a7140a5fba2cf35ebe4d18c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PDMasterDetail")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PDMasterDetail")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
