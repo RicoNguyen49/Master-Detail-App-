3e97933d13229b191ca9b7697013bd4ec5839cd4
H4N4K8 <hannah.francis03@gmail.com>
2024-09-26 16:42:23 -0400

    details and index

--- PDMasterDetail/obj/Release/net8.0/PDMasterDetail.AssemblyInfo.cs
+++ PDMasterDetail/obj/Release/net8.0/PDMasterDetail.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PDMasterDetail")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753013d390787890ea059b38a12632eabcfab1d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PDMasterDetail")]
